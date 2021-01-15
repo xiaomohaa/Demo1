c71f2ce39540e15f73d21dcf6e00ab186cc3c6dc
妍鑫 小 <guangxin_li@outlook.com>
2021-01-15 16:48:10 +0800

    暂定

---
+++ Demo1/00 开始.cpp
@@ -0,0 +1,30 @@
+//#include <iostream>
+//using namespace std;
+//#include <string.h>
+//
+//// 函数声明
+//void JustShow(void);
+//
+//#define Week 7		// 常量不可修改
+//const int Day = 30;
+//
+//int main(void)
+//{	
+//	string str1 = "Hello World";
+//	bool flag = true;
+//
+//	cout << str1 << endl;		// endl 是换行的意思
+//	cout << flag << endl;
+//
+//
+//	JustShow();
+//
+//	//system("pause");
+//	return 0;
+//}
+//
+//void JustShow(void)
+//{
+//	cout << sizeof(Day) << endl;
+//	cout << "Hello XiaoMo \\" << endl;
+//}
---
+++ Demo1/01 输入.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+#include <string>
+
+int main(void)
+{
+	int a = 0;
+	float b = 0.0f;
+	char ch = 'a';
+	string str = "hhh";
+	bool flag = true;
+
+	cout << "Hello XiaoMo !" << endl;
+
+	cout << "请给整型变量a赋值：";
+	cin >> a;
+	cout << "请给浮点型变量b赋值：";
+	cin >> b;
+	cout << "请给字符型变量ch赋值：";
+	cin >> ch;
+	cout << "请给字符串str赋值：";
+	cin >> str;
+	cout << "请给布尔类型的flag赋值：";
+	cin >> flag;
+
+	cout << "整型变量a=" << a << endl;
+	cout << "浮点型变量b=" << b << endl;
+	cout << "字符型变量ch=" << ch << endl;
+	cout << "字符串str=" << str << endl;
+	cout << "布尔类型的flag=" << flag << endl;
+
+	return 0;
+}
